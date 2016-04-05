32fe645b80a0b81f150ac48bdf5a0631a0987542
Cezary Katner <cezary.katner@l-p-a.com>
2016-04-05 19:23:56 +0200

    Add project files.

---
+++ CoffeeTracker/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CoffeeTracker.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ CoffeeTracker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CoffeeTracker.WebApiApplication" Language="C#" %>
