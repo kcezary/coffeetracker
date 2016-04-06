515ed528a3cd693a25e82c8706b9da1663a09d80
Cezary Katner <cezary.katner@l-p-a.com>
2016-04-06 21:58:14 +0200

    more entries while seeding + more coffee types

--- CoffeeTracker/Model/MockDatabaseInitializer.cs
+++ CoffeeTracker/Model/MockDatabaseInitializer.cs
@@ -12,12 +12,13 @@ namespace CoffeeTracker.Model
             base.Seed(context);
 
             var coffeeEntries = new List<CoffeeEntry>();
+            var random = new Random(1000);
 
-            for (int i = 100; i > 0; i--)
+            for (int i = 1000; i > 0; i--)
             {
                 var entry = new CoffeeEntry
                 {
-                    CoffeeType = "Espresso" + (i % 2 == 0 ? " Doppio" : "Macchiato"),
+                    CoffeeType = GetRandomCoffeeType(random),
                     ItemPrice = 2.3M,
                     Location = Faker.Address.City(),
                     Quantity = i % 3 == 0 ? 2 : 1,
@@ -29,5 +30,27 @@ namespace CoffeeTracker.Model
 
             context.BulkInsert(coffeeEntries);
         }
+
+        private static string GetRandomCoffeeType(Random r)
+        {
+            var pos = r.Next(0, CoffeeTypes.Length);
+            return CoffeeTypes[pos];
+        }
+
+        private static readonly string[] CoffeeTypes = new[] 
+        {
+            "Added milk, sweeteners and substitutes", "Drip or filtered coffee", "French press or cafetière",
+            "Percolated", "Turkish coffee", "Cold brew", "Vacuum coffee", "Espresso and variations", "Caffè Americano",
+            "Café Cubano", "Caffè crema", "Cafe Zorro", "Doppio", "Espresso Romano", "Guillermo", "Ristretto",
+            "Espresso with milk", "Antoccino", "Breve", "Café bombón", "Cappuccino", "Cortado", "Latte", "Latte macchiato",
+            "Espressino", "Flat white", "Galão", "Macchiato", "Wiener or Viennese melange", "Coffee with milk",
+            "Café au lait", "Ca phe sua da", "Egg coffee", "Eggnog latte", "Eiskaffee", "Kopi susu", "White coffee",
+            "White coffee (England)", "Coffee or espresso with whipped cream", "Vienna coffee", "Espresso con panna",
+            "Combinations", "Coffee with espresso", "Coffee with tea", "Black tie", "Chai latte", "Red tie", "Yuanyang",
+            "Coffee with alcohol", "Liqueur coffee", "Irish coffee", "Rüdesheimer Kaffee", "Pharisäer", "Carajillo",
+            "Flavoured", "Melya", "Caffè Marocchino", "Café miel", "Mocha or café mocha or mochaccino", "Café de olla",
+            "Miscellaneous", "Iced", "Greek frappé coffee", "Mazagran", "Palazzo", "Ice Shot", "Instant coffee", "Canned coffee",
+            "Coffee milk", "Decaffeinated", "Other", "Affogato", "Caffè Medici", "Café Touba", "Indian filter coffee", "Moka", "Shakerato", "Pocillo"
+        };
     }
 }
